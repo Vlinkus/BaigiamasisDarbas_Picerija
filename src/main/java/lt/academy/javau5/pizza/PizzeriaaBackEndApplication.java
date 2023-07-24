ed4db493ccddeab841b02331b2cafe3505a485f4
VLinkus <linkusvyte@gmail.com>
2023-07-24 15:47:42 +0300

    Initial Commit

---
+++ src/main/java/lt/academy/javau5/pizza/PizzeriaaBackEndApplication.java
@@ -0,0 +1,13 @@
+package lt.academy.javau5.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaaBackEndApplication.class, args);
+	}
+
+}
