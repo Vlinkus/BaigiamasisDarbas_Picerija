2b037757ed80cb784cc17ccb97337594ac2f2360
Gytis <gytispaskauskas@yahoo.com>
2023-08-03 11:12:36 +0300

    languege files

--- src/main/java/lt/academy/javau5/pizza/PizzeriaaBackEndApplication.java
+++ src/main/java/lt/academy/javau5/pizza/PizzeriaaBackEndApplication.java
@@ -3,11 +3,16 @@ package lt.academy.javau5.pizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PizzeriaaBackEndApplication {
 
+@SpringBootApplication
+public class PizzeriaaBackEndApplication  {
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaaBackEndApplication.class, args);
+		
+		
 	}
 	
 	
