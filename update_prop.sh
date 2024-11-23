a399b6372350b47e531c6700d41fbd9cfb5d4968
Maksim Pavlenko <maksimas.pavlenko@gmail.com>
2024-11-23 11:46:23 +0200

    fix: dockerignore 7

--- update_prop.sh
+++ update_prop.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-
+ser -x
 # Define the file path
-APPLICATION_PROPERTIES="src/main/resources/application.properties"
+APPLICATION_PROPERTIES="./src/main/resources/application.properties"
 
 # Check if the file exists
 if [[ -f "$PACKAGE_JSON" ]]; then
     sed -i 's#maxim23#postgres#' "$APPLICATION_PROPERTIES"
     sed -i 's#jdbc:postgresql://localhost:5432#jdbc:postgresql://192.168.10.250:5432#' "$APPLICATION_PROPERTIES"
+    cat "./$APPLICATION_PROPERTIES"
 
     # Confirm the change
     if grep -q 'password=postgres' "$APPLICATION_PROPERTIES"; then
-        echo "Successfully updated password in $APPLICATION_PROPERTIES."
+        echo "Successfully updated pw in $APPLICATION_PROPERTIES."
     else
-        echo "Failed to update the password in $APPLICATION_PROPERTIES."
+        echo "Failed to update the pw in $APPLICATION_PROPERTIES."
         exit 1
     fi
 
     if grep -q 'jdbc:postgresql://192.168.10.250:5432/pizzeria' "$APPLICATION_PROPERTIES"; then
-          echo "Successfully updated password in $APPLICATION_PROPERTIES."
+          echo "Successfully updated db link in $APPLICATION_PROPERTIES."
       else
-          echo "Failed to update the password in $APPLICATION_PROPERTIES."
+          echo "Failed to update the db link in $APPLICATION_PROPERTIES."
           exit 1
       fi
 else
