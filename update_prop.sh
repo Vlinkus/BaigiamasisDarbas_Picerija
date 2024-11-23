ea05c15fe6a99805c506fd8d8a7221475730b3ea
Maksim Pavlenko <maksimas.pavlenko@gmail.com>
2024-11-23 11:56:39 +0200

    fix: dockerignore 9

--- update_prop.sh
+++ update_prop.sh
@@ -4,7 +4,7 @@
 APPLICATION_PROPERTIES="src/main/resources/application.properties"
 
 # Check if the file exists
-if [[ -f "$PACKAGE_JSON" ]]; then
+if [[ -f "$APPLICATION_PROPERTIES" ]]; then
     sed -i 's#maxim23#postgres#' "$APPLICATION_PROPERTIES"
     sed -i 's#jdbc:postgresql://localhost:5432#jdbc:postgresql://192.168.10.250:5432#' "$APPLICATION_PROPERTIES"
     cat "./$APPLICATION_PROPERTIES"
